eb2acd5887a4219732f3154a817e08fef63f0e19
cpwallah <142053202+cpwallah@users.noreply.github.com>
2023-09-13 01:01:05 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+let food1s=document.getElementById('food1');
+let food2s=document.getElementById('food2');
+let foods=document.getElementById('food');
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('images/food1.png')";
+})
